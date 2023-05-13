6a232d58c3e7a38bbab3b7fe4940b90026a7db04
Boz <126583853+nachodevV2@users.noreply.github.com>
2023-05-13 16:24:20 +0100

    Update Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -21,33 +21,9 @@ local Window = Rayfield:CreateWindow({
       Note = "https://discord.gg/g3dt2Z2A",
       FileName = "SofusKey",
       SaveKey = true,
-      GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from - 12819866175
-      Key = "https://pastebin.com/raw/Sc5pRfhJ"
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from - 12819866175
+      Key = "ByfronGG"
    }
 })
 
 local Tab = Window:CreateTab("Home", 4483362458) -- Title, Image
-
-local Section = Tab:CreateSection("do this pls")
-
-
-local Tab = Window:CreateTab("Bedwars", 4483362458) -- Title, Image
-
-local Section = Tab:CreateSection("General")
-
-local Toggle = Tab:CreateToggle({
-   Name = "CFrame Fly",
-   CurrentValue = false,
-   Flag = "CFRAMEFLY", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
-   Callback = function(Value)
-   loadstring(game:HttpGet('https://raw.githubusercontent.com/Lost20/Roblox-Scripts/main/Cframe%20fly'))()
-   end,
-})
-
-local Section = Tab:CreateSection("Blatant")
-
-
-local Section = Tab:CreateSection("Utility")
-
-
-local Section = Tab:CreateSection("World")
