b4d712e7771284bb36707927a6967cb7005202ee
Boz <126583853+nachodevV2@users.noreply.github.com>
2023-05-13 16:12:59 +0100

    Update and rename MainScript.lua to Universal.lua

--- Universal.lua
+++ Universal.lua
@@ -1,24 +1,24 @@
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Sofus.GG | Beta | Public",
-   LoadingTitle = "Sofus Universal",
-   LoadingSubtitle = "Injecting API",
+   Name = "Sophus.GG | Beta | Public",
+   LoadingTitle = "Sophus Universal",
+   LoadingSubtitle = "Injecting",
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = SofusGG, -- Create a custom folder for your hub/game
-      FileName = "Sofus"
+      FolderName = SophusGG, -- Create a custom folder for your hub/game
+      FileName = "Sophus"
    },
    Discord = {
       Enabled = false,
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD.
+      Invite = "g3dt2Z2A", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD.
       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
    },
    KeySystem = true, -- Set this to true to use our key system
    KeySettings = {
       Title = "Sofus.GG",
       Subtitle = "Key System",
-      Note = "http://adfoc.us/80052889186864",
+      Note = "https://discord.gg/g3dt2Z2A",
       FileName = "SofusKey",
       SaveKey = true,
       GrabKeyFromSite = true, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from - 12819866175
